d6dc254e1d935f80b969950e3fa8c578b89dc350
jb-network <85036487+jb-network@users.noreply.github.com>
2022-07-05 23:10:38 -0500

    Add project files.

---
+++ The_Feud/Program.cs
@@ -0,0 +1,26 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 33  "The Feud" Challenge
+// This is a challenge to resolve conflicts in Naming Spaces
+// Used both Aliases and FQ Names
+
+using IField;
+using McDroid;
+
+//Aliases
+using IFieldPig = IField.Pig;
+using McDroidPig = McDroid.Pig;
+
+IFieldPig IPig = new IFieldPig();
+McDroidPig MPig = new McDroidPig();
+
+//Fully Qualified Name
+IField.Goat IGoat = new IField.Goat();
+McDroid.Goat MGoat = new McDroid.Goat();
+
+//Non Shared Names
+Sheep ISheep = new Sheep();
+Cow MCow = new Cow();
+
+Console.WriteLine("All object created with no errors");
+Console.WriteLine("Press any key to end the program");
+Console.ReadKey();
